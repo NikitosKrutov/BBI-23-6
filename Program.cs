3c94194937533844d2b4a8e317273a173c9c0c27
NikitosKrutov <m2315354@edu.misis.ru>
2023-09-30 01:11:09 +0300

    aaa

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("salam popalam");
+            Console.WriteLine("salam alekum");
         }
     }
 }
